d9fff03ebbf145a9b560d6c2927d73ac3d9901b8
luqmansdn <lshe0017@student.monash.edu>
2023-10-09 02:15:42 +1100

    Changes to file names

--- vegalite.js
+++ vegalite.js
@@ -1,4 +1,4 @@
-var vg_1 = "Week9Studio.json";
+var vg_1 = "DataVis2VegaLite.json";
 vegaEmbed("#hospitals", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
