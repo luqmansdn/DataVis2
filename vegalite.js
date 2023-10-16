52fbbc9fcd7fcac5380d82cb809ff2188e423331
luqmansdn <lshe0017@student.monash.edu>
2023-10-17 03:07:40 +1100

    Updates

--- vegalite.js
+++ vegalite.js
@@ -12,3 +12,8 @@ var vg_3 = "HospitalDensityVSPopulation.json";
 vegaEmbed("#HospitalDensityPopulation", vg_3).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_4 = "GDPVSPopulation.json";
+vegaEmbed("#GDPPopulation", vg_4).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
