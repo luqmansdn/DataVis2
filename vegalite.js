a45c8b8618e216ffa41e40222eb6839783a82076
luqmansdn <lshe0017@student.monash.edu>
2023-10-18 00:42:24 +1100

    Updates galore

--- vegalite.js
+++ vegalite.js
@@ -1,19 +1,24 @@
-var vg_1 = "GDPMap.json";
-vegaEmbed("#GDP", vg_1).then(function(result) {
+var vg_1 = "#1_GDP_GDP%.json";
+vegaEmbed("#GDP_GDP%", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_2 = "HospitalDensityVSGDP.json";
-vegaEmbed("#HospitalDensityGDP", vg_2).then(function(result) {
+var vg_2 = "#2_GDPMap.json";
+vegaEmbed("#GDPMap", vg_2).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_3 = "HospitalDensity_Population.json";
-vegaEmbed("#HospitalDensityPopulation", vg_3).then(function(result) {
+var vg_3 = "#3_DevelopedCountries_HospitalDensity.json";
+vegaEmbed("#DevelopedCountries_HospitalDensity", vg_3).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_4 = "GDPVSPopulation.json";
-vegaEmbed("#GDPPopulation", vg_4).then(function(result) {
+var vg_4 = "#4_HospitalDensity_Population.json";
+vegaEmbed("#HospitalDensity_Population", vg_4).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_5 = "#5_DensityHeatMap.json";
+vegaEmbed("#DensityHeatMap", vg_4).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
