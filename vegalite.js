772115b2f9de61112bdd7a2c9570aab0d4b4f474
luqmansdn <lshe0017@student.monash.edu>
2023-10-18 01:39:36 +1100

    Updates

--- vegalite.js
+++ vegalite.js
@@ -1,27 +1,27 @@
-var vg_1 = "#1_GDP_GDP%.json";
+var vg_1 = "1_GDP_GDP%.json";
 vegaEmbed("#GDP_GDP%", vg_1).then(function(result) {
 }).catch(console.error);
 
-var vg_2 = "#2_GDPMap.json";
+var vg_2 = "2_GDPMap.json";
 vegaEmbed("#GDPMap", vg_2).then(function(result) {
 }).catch(console.error);
 
-var vg_3 = "#3_DevelopedCountries_HospitalDensity.json";
+var vg_3 = "3_DevelopedCountries_HospitalDensity.json";
 vegaEmbed("#DevelopedCountries_HospitalDensity", vg_3).then(function(result) {
 }).catch(console.error);
 
-var vg_4 = "#4_HospitalDensity_Population.json";
+var vg_4 = "4_HospitalDensity_Population.json";
 vegaEmbed("#HospitalDensity_Population", vg_4).then(function(result) {
 }).catch(console.error);
 
-var vg_5 = "#5_DensityHeatMap.json";
+var vg_5 = "5_DensityHeatMap.json";
 vegaEmbed("#DensityHeatMap", vg_5).then(function(result) {
 }).catch(console.error);
 
-var vg_6 = "#6_GDPPieChart.json";
+var vg_6 = "6_GDPPieChart.json";
 vegaEmbed("#GDPPieChart", vg_6).then(function(result) {
 }).catch(console.error);
 
-var vg_7 = "#7_GDPPC_Population.json";
+var vg_7 = "7_GDPPC_Population.json";
 vegaEmbed("#GDPPC_Population", vg_7).then(function(result) {
 }).catch(console.error);
